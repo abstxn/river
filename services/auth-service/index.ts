de8c89627efa5db795bd2280b45b8e0fa4ab3c26
abstxn <108669111+abstxn@users.noreply.github.com>
2024-11-20 07:19:46 +0800

    create auth-service, debug styling, port handling

---
+++ services/auth-service/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import dotenv from 'dotenv';
+
+// Import environment variables.
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: "You have reached auth-service." })
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`user-service running on port ${PORT}`);
+});
--- services/user-service/index.ts
+++ services/user-service/index.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
 import userRoutes from './routes/user-routes';
+import dotenv from 'dotenv';
+
+// Import environment variables.
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -9,7 +13,7 @@ app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: "You have reached user-service." })
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`user-service running on port ${PORT}`);
 });

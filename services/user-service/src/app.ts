1309306a9b835b8b1060a716cbc23f60b7052c6a
abstxn <108669111+abstxn@users.noreply.github.com>
2024-11-19 08:42:21 +0800

    setup as basic express app (TS)

---
+++ services/user-service/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('You have reached user-service.');
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`user-service running on http://localhost:${PORT}`);
+});
